f38f61b232dce311b028fab8e1bd53e60959c339
Badeeson <badeeson@gmail.com>
2023-12-11 09:04:07 +0700

    Add: barchart dataset;

---
+++ src/components/DashboardPage/dataset.ts
@@ -0,0 +1,236 @@
+export const datasetNetworks = [
+  {
+    getBio: 445,
+    getLog: 666,
+    call: 434,
+    chain: 753,
+    blockn: 668,
+    time: '09:00',
+  },
+  {
+    getBio: 445,
+    getLog: 666,
+    call: 434,
+    chain: 753,
+    blockn: 668,
+    time: '12:00',
+  },
+  {
+    getBio: 445,
+    getLog: 666,
+    call: 434,
+    chain: 753,
+    blockn: 668,
+    time: '15:00',
+  },
+  {
+    getBio: 771,
+    getLog: 366,
+    call: 434,
+    chain: 753,
+    blockn: 668,
+    time: '18:00',
+  },
+  {
+    getBio: 1500,
+    getLog: 2000,
+    call: 1600,
+    chain: 1443,
+    blockn: 1732,
+    time: '21:00',
+  },
+  {
+    getBio: 889,
+    getLog: 995,
+    call: 993,
+    chain: 753,
+    blockn: 667,
+    time: 'Nov 17',
+  },
+  {
+    getBio: 1511,
+    getLog: 357,
+    call: 975,
+    chain: 1266,
+    blockn: 1578,
+    time: '03:00',
+  },
+  {
+    getBio: 1200,
+    getLog: 1000,
+    call: 1023,
+    chain: 1500,
+    blockn: 600,
+    time: '06:00',
+  },
+];
+
+export const datasetMethods = [
+  {
+    mainnet: 445,
+    sepolia: 666,
+    optimismM: 434,
+    arbitrumM: 753,
+    celoMainnet: 668,
+    time: '09:00',
+  },
+  {
+    mainnet: 445,
+    sepolia: 666,
+    optimismM: 434,
+    arbitrumM: 753,
+    celoMainnet: 668,
+    time: '12:00',
+  },
+  {
+    mainnet: 445,
+    sepolia: 666,
+    optimismM: 434,
+    arbitrumM: 753,
+    celoMainnet: 668,
+    time: '15:00',
+  },
+  {
+    mainnet: 771,
+    sepolia: 366,
+    optimismM: 434,
+    arbitrumM: 753,
+    celoMainnet: 668,
+    time: '18:00',
+  },
+  {
+    mainnet: 1500,
+    sepolia: 2000,
+    optimismM: 1600,
+    arbitrumM: 1443,
+    celoMainnet: 1732,
+    time: '21:00',
+  },
+  {
+    mainnet: 889,
+    sepolia: 995,
+    optimismM: 993,
+    arbitrumM: 753,
+    celoMainnet: 667,
+    time: 'Nov 17',
+  },
+  {
+    mainnet: 1511,
+    sepolia: 357,
+    optimismM: 975,
+    arbitrumM: 1266,
+    celoMainnet: 1578,
+    time: '03:00',
+  },
+  {
+    mainnet: 1200,
+    sepolia: 1000,
+    optimismM: 1023,
+    arbitrumM: 1500,
+    celoMainnet: 600,
+    time: '06:00',
+  },
+];
+
+export const datasetRequests = [
+  {
+    requests: 109,
+    time: '08:00',
+  },
+  {
+    requests: 1450,
+    time: '09:00',
+  },
+  {
+    requests: 1643,
+    time: '10:00',
+  },
+  {
+    requests: 1520,
+    time: '11:00',
+  },
+  {
+    requests: 1400,
+    time: '12:00',
+  },
+  {
+    requests: 1563,
+    time: '13:00',
+  },
+  {
+    requests: 1424,
+    time: '14:00',
+  },
+  {
+    requests: 1466,
+    time: '15:00',
+  },
+  {
+    requests: 1494,
+    time: '16:00',
+  },
+  {
+    requests: 1423,
+    time: '17:00',
+  },
+  {
+    requests: 1750,
+    time: '18:00',
+  },
+  {
+    requests: 2600,
+    time: '19:00',
+  },
+  {
+    requests: 1940,
+    time: '20:00',
+  },
+  {
+    requests: 1750,
+    time: '21:00',
+  },
+  {
+    requests: 1662,
+    time: '22:00',
+  },
+  {
+    requests: 941,
+    time: '23:00',
+  },
+  {
+    requests: 806,
+    time: 'Nov 17',
+  },
+  {
+    requests: 877,
+    time: '01:00',
+  },
+  {
+    requests: 750,
+    time: '02:00',
+  },
+  {
+    requests: 890,
+    time: '03:00',
+  },
+  {
+    requests: 678,
+    time: '04:00',
+  },
+  {
+    requests: 978,
+    time: '05:00',
+  },
+  {
+    requests: 786,
+    time: '06:00',
+  },
+  {
+    requests: 1001,
+    time: '07:00',
+  },
+  {
+    requests: 899,
+    time: '08:00',
+  },
+];
--- src/components/DashboardPage/index.tsx
+++ src/components/DashboardPage/index.tsx
@@ -4,16 +4,27 @@ import * as React from 'react';
 import { Box } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import SummaryBarChart from './SummaryBarChart';
+import { datasetMethods, datasetNetworks, datasetRequests } from './dataset';
 
 export default function DashboardPage() {
   return (
     <Box>
       <Grid container spacing={2}>
-        {/* <Grid xs={6}>
+        <Grid xs={6}>
           <SummaryBarChart
             title="Network Request Volumes"
             hasFilter
             filterTitle="All networks"
+            totalUsed="33,477"
+            dataset={datasetNetworks}
+            xAxis={[{ scaleType: 'band', dataKey: 'time' }]}
+            series={[
+              { dataKey: 'getBio', label: 'eth_getBio' },
+              { dataKey: 'getLog', label: 'eth_getLog' },
+              { dataKey: 'call', label: 'eth_call' },
+              { dataKey: 'chain', label: 'eth_chain' },
+              { dataKey: 'blockn', label: 'eth_blockn' },
+            ]}
           />
         </Grid>
         <Grid xs={6}>
@@ -21,11 +32,28 @@ export default function DashboardPage() {
             title="Method Request Volumes"
             hasFilter
             filterTitle="All methods"
+            totalUsed="33,472"
+            dataset={datasetMethods}
+            xAxis={[{ scaleType: 'band', dataKey: 'time' }]}
+            series={[
+              { dataKey: 'mainnet', label: 'mainnet' },
+              { dataKey: 'sepolia', label: 'sepolia' },
+              { dataKey: 'optimismM', label: 'optimism-m' },
+              { dataKey: 'arbitrumM', label: 'arbitrum-m' },
+              { dataKey: 'celoMainnet', label: 'celo-mainnet' },
+            ]}
           />
-        </Grid> */}
+        </Grid>
         <Grid xs={12}>
           <SummaryBarChart
             title="Requests Volume"
+            totalUsed="33,428"
+            showMaximum
+            dataset={datasetRequests}
+            xAxis={[{ scaleType: 'band', dataKey: 'time', label: 'UTC' }]}
+            series={[
+              { dataKey: 'requests', label: 'Requests' },
+            ]}
           />
         </Grid>
       </Grid>
